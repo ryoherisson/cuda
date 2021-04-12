023c550971b5f5f010c2073305e0f5e752248a0b
ryoherisson <ryhizw@gmail.com>
2021-04-12 23:00:01 +0900

    common

--- src/cuda_master_cpp/common/common.cpp
+++ src/cuda_master_cpp/common/common.cpp
@@ -1,5 +1,69 @@
 #include "common.h"
 
+// simple initialization
+void initialize(int *input, const int array_size, INIT_PARAM PARAM, int x) {
+  if (PARAM == INIT_ONE) {
+    for (int i = 0; i < array_size; i++) {
+      input[i] = 1;
+    }
+  } else if (PARAM == INIT_ONE_TO_TEN) {
+    for (int i = 0; i < array_size; i++) {
+      input[i] = i % 10;
+    }
+  } else if (PARAM == INIT_RANDOM) {
+    time_t t;
+    srand((unsigned)time(&t));
+    for (int i = 0; i < array_size; i++) {
+      input[i] = (int)(rand() & 0xFF);
+    }
+  } else if (PARAM == INIT_FOR_SPARSE_METRICS) {
+    srand(time(NULL));
+    int value;
+    for (int i = 0; i < array_size; i++) {
+      value = rand() % 25;
+      if (value < 5) {
+        input[i] = value;
+      } else {
+        input[i] = 0;
+      }
+    }
+  } else if (PARAM == INIT_0_TO_X) {
+    srand(time(NULL));
+    int value;
+    for (int i = 0; i < array_size; i++) {
+      input[i] = (int)(rand() & 0xFF);
+    }
+  }
+}
+
+void initialize(float *input, const int array_size, INIT_PARAM PARAM) {
+  if (PARAM == INIT_ONE) {
+    for (int i = 0; i < array_size; i++) {
+      input[i] = 1;
+    }
+  } else if (PARAM == INIT_ONE_TO_TEN) {
+    for (int i = 0; i < array_size; i++) {
+      input[i] = i % 10;
+    }
+  } else if (PARAM == INIT_RANDOM) {
+    srand(time(NULL));
+    for (int i = 0; i < array_size; i++) {
+      input[i] = rand() % 10;
+    }
+  } else if (PARAM == INIT_FOR_SPARSE_METRICS) {
+    srand(time(NULL));
+    int value;
+    for (int i = 0; i < array_size; i++) {
+      value = rand() % 25;
+      if (value < 5) {
+        input[i] = value;
+      } else {
+        input[i] = 0;
+      }
+    }
+  }
+}
+
 void compare_arrays(int *a, int *b, int size) {
   for (int i = 0; i < size; i++) {
     if (a[i] != b[i]) {
@@ -8,4 +72,34 @@ void compare_arrays(int *a, int *b, int size) {
     }
   }
   printf("Arrays are same \n");
+}
+
+void compare_arrays(float *a, float *b, int size) {
+  for (int i = 0; i < size; i++) {
+    if (a[i] != b[i]) {
+      printf("Arrays are different \n");
+      return;
+    }
+  }
+  printf("Arrays are same \n");
+}
+
+// cpu reduction
+int reduction_cpu(int *input, const int size) {
+  int sum = 0;
+  for (int i = 0; i < size; i++) {
+    sum += input[i];
+  }
+  return sum;
+}
+
+void compare_results(int gpu_result, int cpu_result) {
+  printf("GPU result: %d, CPU result: %d \n", gpu_result, cpu_result);
+
+  if (gpu_result == cpu_result) {
+    printf("GPU and CPU results are same \n");
+    return;
+  }
+
+  printf("GPU and CPU results are different \n");
 }
--- src/cuda_master_cpp/common/common.h
+++ src/cuda_master_cpp/common/common.h
@@ -1,9 +1,37 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <cstring>
+#include <fstream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+enum INIT_PARAM {
+  INIT_ZERO,
+  INIT_RANDOM,
+  INIT_ONE,
+  INIT_ONE_TO_TEN,
+  INIT_FOR_SPARSE_METRICS,
+  INIT_0_TO_X
+};
+
+// simple initialization
+void initialize(int *input, const int array_size,
+                INIT_PARAM PARAM = INIT_ONE_TO_TEN, int x = 0);
+
+void initialize(float *input, const int array_size,
+                INIT_PARAM PARAM = INIT_ONE_TO_TEN);
+
+// reduction in cpu
+int reduction_cpu(int *input, const int size);
+
+// compare results
+void compare_results(int gpu_result, int cpu_result);
 
 // compare two arrays
 void compare_arrays(int *a, int *b, int size);
+
+void compare_arrays(float *a, float *b, int size);
 
 #endif // !COMMON_H
