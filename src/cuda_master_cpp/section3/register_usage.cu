c7a3e9f670929bf9b140f22d4e648284fae77ff3
ryoherisson <ryhizw@gmail.com>
2021-04-18 17:03:42 +0900

    register usage

---
+++ src/cuda_master_cpp/section3/register_usage.cu
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+__global__ void register_usage_test(int *results, int size) {
+  int gid = blockDim.x * blockIdx.x + threadIdx.x;
+
+  int x1 = 3456;
+  int x2 = 1768;
+  int x3 = 453;
+  int x4 = x1 + x2 + x3;
+
+  if (gid < size) {
+    results[gid] = x4;
+  }
+};
+
+int main() {
+  int size = 1 << 22;
+  int byte_size = sizeof(int) * size;
+
+  int *h_ref = (int *)malloc(byte_size);
+  int *d_results;
+  cudaMalloc((void **)&d_results, byte_size);
+  cudaMemset(d_results, 0, byte_size);
+
+  dim3 blocks(128);
+  dim3 grid((size + blocks.x - 1) / blocks.x);
+
+  printf("launching the kernel \n");
+  register_usage_test<<<grid, blocks>>>(d_results, size);
+  cudaDeviceSynchronize();
+
+  return 0;
+}
